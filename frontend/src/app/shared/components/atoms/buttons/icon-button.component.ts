8558702a5be181147bd8ef8fcb0fc6964c61e189
Cesar López <utm24090599@utma.edu.mx>
2025-10-24 13:59:10 -0600

    Feature(.postcss.json): He agregado el FrameWork Tailwindcss

--- frontend/src/app/shared/components/atoms/buttons/icon-button.component.ts
+++ frontend/src/app/shared/components/atoms/buttons/icon-button.component.ts
@@ -28,7 +28,7 @@ import { NgClass } from '@angular/common';
       border: none;
       cursor: pointer;
       transition: all 0.2s ease-in-out;
-      dispaly: flex;
+      display: flex;
       align-items: center;
       justify-content: center;
       padding: 0.5rem;
